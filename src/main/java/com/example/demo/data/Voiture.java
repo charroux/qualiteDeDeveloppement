25852296b2a97796b9eef35e9c98cf0078ee0687
Rayane95210 <99320163+Rayane95210@users.noreply.github.com>
2023-03-31 13:23:28 +0200

    Update Voiture.java

--- src/main/java/com/example/demo/data/Voiture.java
+++ src/main/java/com/example/demo/data/Voiture.java
@@ -4,3 +4,45 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+@Entity
+public class Voiture {
+    int id;
+    String marque;
+    double prix;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    @Override
+    public String toString() {
+        return "Voiture{" +
+                "id=" + id +
+                ", marque='" + marque + ''' +
+        ", prix=" + prix +
+                '}';
+    }
+}
