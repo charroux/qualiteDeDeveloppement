2d8ba85abb36a03fa280c276392adc9a40b22d19
charroux <benoit.charroux@gmail.com>
2024-06-03 14:57:19 +0200

    newcarservice

---
+++ src/main/java/com/example/demo/service/SatistiqueImpl.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import com.example.demo.data.Voiture;
+
+public class SatistiqueImpl implements Statistique{
+    @Override
+    public void ajouter(Voiture voiture) {
+        
+    }
+
+    @Override
+    public int prix() throws ArithmeticException {
+        return 0;
+    }
+}
